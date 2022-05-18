46b5a788c5ff7b275c5d7c0a0d313922bf872bac
Laye Mehta <layeamehta@gmail.com>
2022-05-17 20:26:23 -0600

    added useEffect hook such that if session cookie present on browser, user automatically goes to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
 import ToggleColourMode from "./components/ToggleColourMode";
-import {BrowserRouter} from "react-router-dom"; 
 import Views from "./components/Views";
 import { AccountContextProvider } from "./components/context/AccountContext";
+
 
 
 function App() {
   return (
     <AccountContextProvider>
-    <BrowserRouter>
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
           <div className="App">
@@ -17,7 +16,6 @@ function App() {
             <ToggleColourMode></ToggleColourMode>
           </div>
       </ChakraProvider>
-    </BrowserRouter>
     </AccountContextProvider>
   );
 }
--- client/src/components/context/AccountContext.js
+++ client/src/components/context/AccountContext.js
@@ -1,9 +1,47 @@
-import React, { createContext, useState } from 'react'; 
+import React, { createContext, useEffect, useState } from 'react'; 
+import { useNavigate } from 'react-router-dom';
 
 export const AccountContext = createContext(); 
 
 export const AccountContextProvider = (props) => {
     const [user, setUser] = useState({loggedIn: false}); 
+    const navigate = useNavigate(); 
+
+    useEffect(() => {
+        const checkSessionExists = async () => {
+            try {
+                const response = await fetch("http://localhost:4000/auth/login", {
+                    credentials: "include"
+                }); 
+
+                if(!response || !response.ok || response.status >= 400){
+                    setUser({loggedIn: false}); 
+                    return; 
+                }
+
+                const data = await response.json(); 
+                if(!data){
+                    setUser({loggedIn: false}); 
+                    return; 
+                }
+
+                console.log(data)
+
+                setUser({...data}); 
+                navigate("/home"); 
+                
+            } catch (err) {
+                setUser({loggedIn: false}); 
+                return; 
+                
+            }
+        }
+
+        checkSessionExists(); 
+    }, [])
+    
+
+
 
     return (
         <AccountContext.Provider
